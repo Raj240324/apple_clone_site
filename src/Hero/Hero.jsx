dfd6ab31cb76c2cfaa60e291a39f19dfe454e170
Nagarajan <naga240324@gmail.com>
2024-11-29 22:18:04 +0530

    Update Hero.jsx

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
      
        <Button type="primary" onClick={showMoreModal} className="more-btn"> Learn more </Button> 
             <Modal title="Product Details" visible={isMoreModalVisible} onOk={morehandleOk} onCancel={morehandleCancel} > 
-              <img src={iphone16pro} alt="Product" style={{ width: '100%',maxWidth:"250px", height: "250px", margin:"0 auto"}} />
+              <img src={iphone16pro} alt="Product" style={{ width: '100%',maxWidth:"250px", height: "250px", margin:"auto"}} />
               <p><strong>1.Powerful Performance: </strong>
               Equipped with M-series chips for blazing-fast speed, multitasking, and professional-grade tasks.</p>
               <p><strong>2. Stunning Display: </strong>
