e00bcb43c9bec14cc7665240ba8cc90ad9804d73
Rajnag24324 <nagarajanindirathi7991@gmail.com>
2024-12-03 19:01:46 +0530

    navbar hover animation

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,48 +1,290 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import apple_logo from "../assets/apple_logo.svg";
 import search from "../assets/search.png";
 import bag from "../assets/bag.png";
 
 const Navbar = () => {
-  const [visible, setVisible] = useState(true);
-  const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [dropdowns, setDropdowns] = useState({
+    store : false,
+    mac : false,
+    iPad : false,
+    iPhone : false,
+    watch : false,
+    airpods : false,
+    tv : false,
+    entertainment : false,
+    accessories : false,
+    support : false,
+  });
 
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) {
-      setVisible(false);
-    } else {
-      setVisible(true);
-    }
-    setLastScrollTop(scrollTop <= 0 ? 0 : scrollTop);
-  };
+  const handleMouseEnter = (item) => {
+    setDropdowns((prevState)=>({prevState, [item]: true}));
+  }
+  const handleMouseLeave = (item) => {
+    setDropdowns((prevState)=>({prevState, [item]: false}));
+  }
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [lastScrollTop]);
+
+
+ 
 
   return (
     <>
-      <div className={`navbar-container ${visible ? "" : "hidden"}`}>
+      <div className="navbar-container">
         <div className="apple-logo">
           <img className="logo" src={apple_logo} alt="Apple Logo" />
         </div>
 
         <ul className="list-container">
-          <li>Store</li>
-          <li>Mac</li>
-          <li>iPad</li>
-          <li>iPhone</li>
-          <li>Watch</li>
-          <li>AirPods</li>
-          <li>TV & Home</li>
-          <li>Entertainment</li>
-          <li>Accessories</li>
-          <li>Support</li>
+        <li
+          onMouseEnter = {()=>handleMouseEnter('store')}
+          > 
+          <a href="#">Store</a>
+         
+          {dropdowns.store && (
+            
+
+            <div className="dropdown-container" onMouseLeave = {()=>handleMouseLeave('store')}>
+              
+            <ul className="dropdown1">
+              <li>Shop</li>
+              <li><a href="#">Shop the Latest</a></li>
+              <li><a href="#">Mac</a></li>
+              <li><a href="#">iPad</a></li>
+              <li><a href="#">iPhone</a></li>
+              <li><a href="#">Apple Watch</a></li>
+              <li><a href="#">Accessories</a></li>
+            </ul>
+          
+            <ul className="dropdown2">
+              <li>Quick Links</li>
+              <li><a href="#">Find a Store</a></li>
+              <li><a href="#">Order Status</a></li>
+              <li><a href="#">Ways to buy</a></li>
+             
+            </ul>
+            <ul className="dropdown3">
+              <li>Shop Special Stores</li>
+              <li><a href="#">Education</a></li>
+              <li><a href="#">Business</a></li>
+  
+             
+            </ul>
+            
+            </div>
+          ) } 
+          
+          </li>
+  
+    
+          <li
+          onMouseEnter = {()=>handleMouseEnter('mac')}
+          ><a href="#">Mac</a>
+          {dropdowns.mac && (
+            
+
+            <div className="dropdown-container" onMouseLeave = {()=>handleMouseLeave('mac')}>
+              
+            <ul className="dropdown1">
+              <li>Explore Mac</li>
+              <li><a href="#">Explore All Mac</a></li>
+              <li><a href="#">MacBook Air</a></li>
+              <li><a href="#">MacBook Pro</a></li>
+              <li><a href="#">iMac</a></li>
+              <li><a href="#">Mac Mini</a></li>
+              <li><a href="#">Mac Studio</a></li>
+              <li><a href="#">Mac Pro</a></li>
+              <li><a href="#">Displays</a></li>
+              <li><strong style={{color:"white"}}>Compare Mac</strong></li>
+              <li><strong style={{color:"white"}}>Switch from PC to Mac</strong></li>
+            </ul>
+          
+            <ul className="dropdown2">
+              <li>Shop Mac</li>
+              <li><a href="#">Shop Mac</a></li>
+              <li><a href="#">Mac Accessories</a></li>
+              <li><a href="#">Ways to buy</a></li>
+             
+            </ul>
+            <ul className="dropdown3">
+
+
+              <li>More from Mac</li>
+                <li>
+                  <a href="#">Mac Support</a>
+                </li>          
+                <li><a href="#">AppleCare+ for Mac</a></li>
+                <li>
+                  <a href="#">macOS Sequoia</a>
+                </li>            
+                <li><a href="#">Apple Intelligence</a></li>
+                <li>
+                  <a href="#">Apps by Apple</a>
+                </li>            
+                <li>
+                  <a href="#">Continuity</a>
+                </li>         
+                <li>
+                  <a href="#">iCloud+</a>
+                </li>      
+                <li>
+                  <a href="#">Mac for Business</a>
+                </li>               
+                <li>
+                  <a href="#">Education</a>
+                </li>        
+            </ul>
+            
+            </div>
+          )}
+          
+          
+          </li>
+
+          <li
+          onMouseEnter = {()=>handleMouseEnter('iPad')}
+
+          
+          ><a href="#">iPad</a>
+          {dropdowns.iPad && (
+            
+
+            <div className="dropdown-container" onMouseLeave = {()=>handleMouseLeave('iPad')}>
+              
+            <ul className="dropdown1">
+            <li>Explore iPad</li>
+               <li><a href="#">Explore All iPad</a></li>
+               <li>
+                 <a href="#">iPad Pro</a>
+               </li>        <li>
+                 <a href="#">iPad Air</a>
+               </li>        <li>
+                 <a href="#">iPad</a>
+               </li>    <li>
+                 <a href="#">iPad mini</a>
+               </li>         <li>
+                 <a href="#">Apple Pencil</a>
+               </li>            <li>
+                 <a href="#">Keyboards</a>
+               </li>         <li>
+                 <strong style={{color:"white"}}>Compare iPad</strong>
+               </li>            <li>
+                 <strong style={{color:"white"}}>Why iPad</strong>
+               </li>     </ul>
+          
+            <ul className="dropdown2">
+            <li>Shop iPad</li>
+            <li>
+              <a href="#">Shop iPad</a>
+            </li>      
+            <li><a href="#">iPad Accessories</a></li>
+            <li>
+              <a href="#">Ways to Buy</a>
+            </li>         
+            </ul>
+
+
+            <ul className="dropdown3">
+            <li>More from iPad</li>
+            <li>
+              <a href="#">iPad Support</a>
+            </li>      
+            <li><a href="#">AppleCare+ for iPad</a></li>
+            <li>
+              <a href="#">iPadOS 18</a>
+            </li>   
+            <li><a href="#">Apple Intelligence</a></li>
+            <li>
+              <a href="#">Apps by Apple</a>
+            </li>       
+            <li>
+              <a href="#">iCloud+</a>
+            </li> 
+            <li>
+              <a href="#">Education</a>
+            </li>       
+            </ul>
+            
+            </div>
+          )}
+          
+          </li>
+          <li
+          onMouseEnter = {()=>handleMouseEnter('iPhone')}
+
+          
+          ><a href="#">iPhone</a>
+          {dropdowns.iPhone && (
+            
+
+            <div className="dropdown-container" onMouseLeave = {()=>handleMouseLeave('iPhone')}>
+              
+            <ul className="dropdown1">
+            <li>Explore iPhone</li>
+
+               <li><a href="#">Explore All iPhone</a></li>
+               <li>
+                 <a href="#">iPhone 16 Pro</a>
+               </li>          <li>
+                 <a href="#">iPhone 16</a>
+               </li>      <li>
+                 <a href="#">iPhone 15</a>
+               </li>      <li>
+                 <a href="#">iPhone 14</a>
+               </li>      <li>
+                 <a href="#">iPhone SE</a>
+               </li>      <li>
+                 <strong style={{color:"white"}}>Compare iPhone</strong>
+               </li>           <li><strong style={{color:"white"}}>Switch from Android</strong></li>     
+               </ul>
+          
+            <ul className="dropdown2">
+            <li>Shop iPhone</li>
+             <li>
+               <a href="#">Shop iPhone</a>
+             </li>      <li><a href="#">iPhone Accessories</a></li>
+             <li>
+               <a href="#">Apple Trade In</a>
+             </li>          <li>
+               <a href="#">Ways to Buy</a>
+             </li>       </ul>
+
+
+            <ul className="dropdown3">
+            <li>More from iPhone</li>
+            <li>
+              <a href="#">iPhone Support</a>
+            </li>      <li><a href="#">AppleCare+ for iPhone</a></li>
+            <li>
+              <a href="#">
+                iOS 18
+                Apple Intelligence
+              </a>
+            </li>          <li>
+              <a href="#">Apps by Apple</a>
+            </li>     <li>
+              <a href="#">iPhone Privacy</a>
+            </li>      
+            <li>
+              <a href="#">
+                iCloud+ </a></li>
+                <li><a href="#">Wallet</a></li>
+                <li><a href="#">Siri</a></li>
+             
+             </ul>
+            
+            </div>
+          )}
+          
+          </li>
+       
+          <li><a href="#">Watch</a></li>
+          <li><a href="#">AirPods</a></li>
+          <li><a href="#">TV & Home</a></li>
+          <li><a href="#">Entertainment</a></li>
+          <li><a href="#">Accessories</a></li>
+          <li><a href="#">Support</a></li>
         </ul>
 
         <div className="search-bag-container">
